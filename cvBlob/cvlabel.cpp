db4034cd3d90ed58ce96ee0e6a7a975891c77fdb
grendel.ccl <grendel.ccl@bc8d7a20-1756-0410-a9eb-53d2785c4341>
2008-09-08 08:20:06 +0000

    Update license.
    git-svn-id: http://cvblob.googlecode.com/svn/trunk@6 bc8d7a20-1756-0410-a9eb-53d2785c4341

--- cvBlob/cvlabel.cpp
+++ cvBlob/cvlabel.cpp
@@ -1,22 +1,21 @@
-/***************************************************************************
- *   Copyright (C) 2007 by Cristóbal Carnero Liñán                         *
- *   grendel.ccl@gmail.com                                                 *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
- ***************************************************************************/
+// Copyright (C) 2007 by Cristóbal Carnero Liñán
+// grendel.ccl@gmail.com
+//
+// This file is part of cvBlob.
+//
+// cvBlob is free software: you can redistribute it and/or modify
+// it under the terms of the Lesser GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// cvBlob is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// Lesser GNU General Public License for more details.
+//
+// You should have received a copy of the Lesser GNU General Public License
+// along with cvBlob.  If not, see <http://www.gnu.org/licenses/>.
+//
 
 #include <stdexcept>
 #include <iostream>
@@ -35,6 +34,8 @@ using namespace std;
 
 //#define NULL 0L
 
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Based on http://en.wikipedia.org/wiki/Disjoint-set_data_structure
 void makeSet(CvBlob *x)
 {
   x->_parent=NULL;
@@ -66,6 +67,7 @@ CvBlob *merge(CvBlob *x, CvBlob *y)
     xRoot->_rank+=1;
   }
 }
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
 unsigned int cvLabel (IplImage *img, IplImage *imgOut, CvBlobs &blobs)
 {
