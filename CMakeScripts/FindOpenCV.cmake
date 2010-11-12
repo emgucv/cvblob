ba2e06aa5a56f16e3b5f14d6545d6b37c238a56a
grendel.ccl <grendel.ccl@bc8d7a20-1756-0410-a9eb-53d2785c4341>
2010-11-12 12:18:45 +0000

    "FindOpen.cmake" improved. Checking for "CV_BASE" variable.
    git-svn-id: http://cvblob.googlecode.com/svn/trunk@271 bc8d7a20-1756-0410-a9eb-53d2785c4341

--- CMakeScripts/FindOpenCV.cmake
+++ CMakeScripts/FindOpenCV.cmake
@@ -20,83 +20,64 @@
 include(UsePkgConfig OPTIONAL)
 
 if (WIN32)
-find_path(OpenCV_cv_INCLUDE_DIR opencv/cv.h
-	c:/Archivos de programa/OpenCV/cv/include
-	c:/Program Files/OpenCV/cv/include
-	c:/Archivos de programa/OpenCV/include/opencv
-	c:/Program Files/OpenCV/include/opencv
-)
+#if ("${CMAKE_SYSTEM_NAME}" MATCHES "Windows")
+  message(STATUS "Windows detected.")
+  if (CV_BASE)
+    set(CV_BASE_INCLUDE ${CV_BASE}/include)
+    set(CV_BASE_LIB ${CV_BASE}/lib)
+  else (CV_BASE)
+    set(CV_BASE_INCLUDE 
+                        c:/OpenCV2.1/include
+                        c:/Archivos de programa/OpenCV/cv/include
+                        c:/Program Files/OpenCV/cv/include
+                        c:/Archivos de programa/OpenCV/include/opencv
+                        c:/Program Files/OpenCV/include/opencv
+    )
+    set(CV_BASE_LIB
+                    c:/OpenCV2.1/lib
+                    c:/Archivos de programa/OpenCV/lib
+                    c:/Program Files/OpenCV/lib
+    ) 
+  endif (CV_BASE)
 
-find_path(OpenCV_cxcore_INCLUDE_DIR opencv/cxcore.h
-	c:/Archivos de programa/OpenCV/cxcore/include
-	c:/Program Files/OpenCV/cxcore/include
-	c:/Archivos de programa/OpenCV/include/opencv
-	c:/Program Files/OpenCV/include/opencv
-)
+  find_path(OpenCV_cv_INCLUDE_DIR opencv/cv.h ${CV_BASE_INCLUDE})
+  find_path(OpenCV_cxcore_INCLUDE_DIR opencv/cxcore.h ${CV_BASE_INCLUDE})
+  find_path(OpenCV_highgui_INCLUDE_DIR opencv/highgui.h ${CV_BASE_INCLUDE})
+  set(OpenCV_INCLUDE_DIR ${OpenCV_cv_INCLUDE_DIR} ${OpenCV_cxcore_INCLUDE_DIR} ${OpenCV_highgui_INCLUDE_DIR})
 
-
-find_path(OpenCV_highgui_INCLUDE_DIR opencv/highgui.h
-	c:/Archivos de programa/OpenCV/otherlibs/highgui
-	c:/Program Files/OpenCV/otherlibs/highgui
-	c:/Archivos de programa/OpenCV/include/opencv
-	c:/Program Files/OpenCV/include/opencv
-)
-
-set(OpenCV_INCLUDE_DIR $(OpenCV_cv_INCLUDE_DIR) $(OpenCV_cxcore_INCLUDE_DIR) $(OpenCV_highgui_INCLUDE_DIR)) 
-
-find_library(OpenCV_cv_LIBRARY
-              NAMES cv
-              PATH c:/Archivos de programa/OpenCV/lib
-                   c:/Program Files/OpenCV/lib
-)
-
-find_library(OpenCV_cxcore_LIBRARY
-              NAMES cxcore
-              PATH c:/Archivos de programa/OpenCV/lib
-                   c:/Program Files/OpenCV/lib
-)
-
-find_library(OpenCV_highgui_LIBRARY
-              NAMES highgui
-              PATH c:/Archivos de programa/OpenCV/lib
-                   c:/Program Files/OpenCV/lib
-)
-
-find_library(OpenCV_cvaux_LIBRARY
-              NAMES cvaux
-              PATH c:/Archivos de programa/OpenCV/lib
-                   c:/Program Files/OpenCV/lib
-)
+  #message(STATUS "CV_BASE_LIB: ${CV_BASE_LIB}")
+  find_library(OpenCV_cv_LIBRARY NAMES cv210 cv PATH ${CV_BASE_LIB})
+  find_library(OpenCV_cxcore_LIBRARY NAMES cxcore cxcore210 PATH ${CV_BASE_LIB})
+  find_library(OpenCV_highgui_LIBRARY NAMES highgui highgui210 PATH ${CV_BASE_LIB})
+  find_library(OpenCV_cvaux_LIBRARY NAMES cvaux cvaux210 PATH ${CV_BASE_LIB})
+  #message(STATUS ${OpenCV_cv_LIBRARY})
+  #message(STATUS ${OpenCV_cxcore_LIBRARY})
+  #message(STATUS ${OpenCV_highgui_LIBRARY})
+  #message(STATUS ${OpenCV_cvaux_LIBRARY})
 else (WIN32)
-find_path(OpenCV_INCLUDE_DIR opencv/cv.h
-          /usr/include
-          /usr/local/include
-)
+#else ("${CMAKE_SYSTEM_NAME}" MATCHES "Windows")
+  message(STATUS "Linux detected.")
+  if (CV_BASE)
+    set(CV_BASE_INCLUDE ${CV_BASE}/include)
+    set(CV_BASE_LIB ${CV_BASE}/lib)
+  else (CV_BASE)
+    set(CV_BASE_INCLUDE 
+                        /usr/include
+                        /usr/local/include
+    )
+    set(CV_BASE_LIB
+                    /usr/lib
+                    /usr/local/lib
+    ) 
+  endif (CV_BASE)
 
-find_library(OpenCV_cv_LIBRARY
-              NAMES cv cv0.9.7 cv0.9.8 cv1.0.0
-              PATH /usr/lib
-                   /usr/local/lib
-)
-
-find_library(OpenCV_cxcore_LIBRARY
-              NAMES cxcore cxcore0.9.7 cxcore0.9.8 cxcore1.0.0
-              PATH /usr/lib
-                   /usr/local/lib
-)
-
-find_library(OpenCV_highgui_LIBRARY
-              NAMES highgui highgui0.9.7 highgui0.9.8 highgui1.0.0
-              PATH /usr/lib
-                   /usr/local/lib
-)
-
-find_library(OpenCV_cvaux_LIBRARY
-              NAMES cvaux cvaux0.9.7 cvaux0.9.8 cvaux1.0.0
-              PATH /usr/lib
-                   /usr/local/lib
-)
+  find_path(OpenCV_INCLUDE_DIR opencv/cv.h ${CV_BASE_INCLUDE})
+  find_library(OpenCV_cv_LIBRARY NAMES cv cv0.9.7 cv0.9.8 cv1.0.0 PATH ${CV_BASE_LIB})
+  find_library(OpenCV_cxcore_LIBRARY NAMES cxcore cxcore0.9.7 cxcore0.9.8 cxcore1.0.0 PATH ${CV_BASE_LIB})
+  find_library(OpenCV_highgui_LIBRARY NAMES highgui highgui0.9.7 highgui0.9.8 highgui1.0.0 PATH ${CV_BASE_LIB})
+  find_library(OpenCV_cvaux_LIBRARY NAMES cvaux cvaux0.9.7 cvaux0.9.8 cvaux1.0.0 PATH ${CV_BASE_LIB})
 endif (WIN32)
+#endif ("${CMAKE_SYSTEM_NAME}" MATCHES "Windows")
 
 if (OpenCV_cv_LIBRARY AND OpenCV_cxcore_LIBRARY AND OpenCV_highgui_LIBRARY AND OpenCV_cvaux_LIBRARY)
   set(OpenCV_LIBRARIES ${OpenCV_cv_LIBRARY} ${OpenCV_cxcore_LIBRARY} ${OpenCV_highgui_LIBRARY} ${OpenCV_cvaux_LIBRARY})
@@ -105,10 +86,10 @@ endif (OpenCV_cv_LIBRARY AND OpenCV_cxcore_LIBRARY AND OpenCV_highgui_LIBRARY AN
 
 if (OpenCV_FOUND)
   if (NOT OpenCV_FIND_QUIETLY)
-    message(STATUS "OpenCV encontrado: ${OpenCV_LIBRARIES}")
+    message(STATUS "OpenCV found: ${OpenCV_LIBRARIES}.")
   endif (NOT OpenCV_FIND_QUIETLY)
 else (OpenCV_FOUND)
   if (OpenCV_FIND_REQUIRED)
-    message(FATAL_ERROR "No se puede encontrar OpenCV")
+    message(FATAL_ERROR "OpenCV not found.")
   endif (OpenCV_FIND_REQUIRED)
 endif (OpenCV_FOUND)
